a85b153fa0ea2e371717946506b8e93447e182fd
Zuzana Protivňaková <z.protivnakova@yahoo.com>
2018-10-29 22:05:00 +0100

    Added chrometest

---
+++ Automaty/src/test/java/ChromeTest.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class ChromeTest {
+
+
+    public ChromeTest(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Zuzana Protivňaková\\Desktop\\Automatization\\Webdrivers\\chromedriver 2.43.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.sk/");
+
+    }
+}
